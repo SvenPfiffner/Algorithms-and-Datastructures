06d038ba59f61820e1a960ef13650b1393c91d56
Sven Pfiffner <svenp@student.ethz.ch>
2019-01-15 10:15:51 +0100

    Minor improvements to Dijkstra and its Querry Functions

--- Algorithms and Datastructures/src/algorithms/Dijkstra.java
+++ Algorithms and Datastructures/src/algorithms/Dijkstra.java
@@ -9,6 +9,7 @@
 package algorithms;
 
 import java.util.PriorityQueue;
+import java.util.Stack;
 
 import datastructures.DistanceVertex;
 import datastructures.Edge;
@@ -35,8 +36,66 @@ public class Dijkstra {
 		}
 		distTo[s] = 0; //Set distance to root to 0
 
-		pq.add(new DistanceVertex(s, 0.0));
+		pq.add(new DistanceVertex(s, 0.0)); 	//Add first vertex with distance 0 to queue
+		while(!pq.isEmpty()) { 					//Go trough queue
+			relax(graph, pq.remove()); 			//Relax and remove first element
+		}
 		
+	}
+	
+	private void relax(WeightedGraph graph, DistanceVertex v) {
+		
+		int vIndex = v.getVertexIndex(); 							//Get Index of DistanceVertex v
+		for(Edge e : graph.adj(vIndex)) { 							//Go trough all edges incident to v
+			int wIndex = e.other(vIndex);							//Get vertex this edge connects to v
+			if(distTo[wIndex] > distTo[vIndex] + e.getWeight()) {	//If distance to w is currently bigger than distance to v + weight of edge
+				distTo[wIndex] = distTo[vIndex] + e.getWeight();	//Set v + weight of edge as new distance
+				edgeTo[wIndex] = e;									//Set current edge as edge that leads to w on the path
+				
+				for(DistanceVertex vertex: pq) { 					//Go trough priority queue
+					if(vertex.getVertexIndex() == wIndex) { 		//If w is part of queue
+						pq.remove(vertex); 							//Remove from queue
+						vertex.setDistanceTo(distTo[wIndex]); 		//Change distance
+						pq.add(vertex); 							//Re-add to queue
+						return; 									//relaxation done
+					}
+				}
+				pq.add(new DistanceVertex(wIndex, distTo[wIndex]));	//Add w to priority queue
+			}
+			
+		}
+	}
+	
+	/**
+	 * Returns shortest possible distance from source to any other vertex
+	 * If this vertex is not reachable by source, return Double.POSITIVE_INFINITY
+	 */
+	public double distTo(int v) {
+		return distTo[v];
+	}
+	
+	/**
+	 * Checks whether a path from source to any other vertex exists
+	 * @return
+	 * True if path exist, false if not
+	 */
+	public boolean hasPathTo(int v) {
+		return(distTo[v] < Double.POSITIVE_INFINITY);
+	}
+	
+	/**
+	 * Returns the shortest path from source to any vertex v as a stack of edges.
+	 * If no such path exist, return null
+	 */
+	public Iterable<Edge> pathTo(int v) {
+		if(!hasPathTo(v)) { 											//Check if path does exist
+			return null;
+		}
+		Stack<Edge> path = new Stack<Edge>(); 							//Create stack which contains path-edges
+		for(Edge e = edgeTo[v]; e != null; e = edgeTo[e.either()]) { 	//Add all edges of the path to stack
+			path.push(e);
+		}
+		return path;													//Return the path
 	}
 	
 	
